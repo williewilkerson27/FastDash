e83ef5d948a2911073ffe2328f465de8c57de75f
Wilky <williamwilkerson27@outlook.com>
2022-12-05 16:16:17 -0500

    4th commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components'; 
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages'; 
 
 import './app.css';
 
@@ -31,7 +34,7 @@ const App = () => {
                         <div className='w-72 fixed sidebar
                         dark:bg-secondary-dark-bg
                         bg-white'>
-                            Sidebar
+                            <Sidebar />
                         </div>
                     ) : (
                         <div className='w-0
@@ -51,29 +54,29 @@ const App = () => {
                         <Routes>
                             {/* element is what you render, right now its a string 'ecommerce */}
                             {/* Dashboard */}
-                            <Route path='/' element='eCommerce' />
-                            <Route path='/ecommerce' element='eCommerce' />
+                            <Route path='/' element={<eCommerce />} />
+                            <Route path='/ecommerce' element={<eCommerce />} />
 
                             {/* Pages */}
-                            <Route path='/orders' element='Orders' />
-                            <Route path='/employees' element='Employees' />
-                            <Route path='/customers' element='Customers' />
+                            <Route path='/orders' element={<Orders />} />
+                            <Route path='/employees' element={<Employees />} />
+                            <Route path='/customers' element={<Customers />} />
 
                             {/* Apps */}
-                            <Route path='/kanban' element='Kanban' />
-                            <Route path='/editor' element='Editor' />
-                            <Route path='/calendar' element='Calendar' />
-                            <Route path='/color-picker' element='ColorPicker' />
+                            <Route path='/kanban' element={<Kanban />} />
+                            <Route path='/editor' element={<Editor />} />
+                            <Route path='/calendar' element={<Calendar />} />
+                            <Route path='/color-picker' element={<ColorPicker />} />
 
                             {/* Charts */}
-                            <Route path='/line' element='Line' />
-                            <Route path='/area' element='Area' />
-                            <Route path='/bar' element='Bar' />
-                            <Route path='/pie' element='Pie' />
-                            <Route path='/financial' element='Financial' />
-                            <Route path='/color-mapping' element='ColorMapping' />
-                            <Route path='/pyramid' element='Pyramid' />
-                            <Route path='/stacked' element='Stacked' />
+                            <Route path='/line' element={<Line />} />
+                            <Route path='/area' element={<Area />} />
+                            <Route path='/bar' element={<Bar />} />
+                            <Route path='/pie' element={<Pie />} />
+                            <Route path='/financial' element={<Financial />} />
+                            <Route path='/color-mapping' element={<ColorMapping />} />
+                            <Route path='/pyramid' element={<Pyramid />} />
+                            <Route path='/stacked' element={<Stacked />} />
                         </Routes>
                     </div>
                     
@@ -86,4 +89,4 @@ const App = () => {
     )
 }
 
-export default App
+export default App;
